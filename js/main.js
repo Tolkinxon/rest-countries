8460526d7062ae1b78f642175b7fa3a7da504e37
Soliyev Tolkinxon <krosstony639@gmail.com>
2024-10-27 23:25:42 +0500

    Add select img

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const elFormSelectWrapper = document.querySelector('.js-form__select-wrapper');
+
+
+
+elFormSelectWrapper.addEventListener('click',() => elFormSelectWrapper.children[0].classList.toggle('rotate'))
