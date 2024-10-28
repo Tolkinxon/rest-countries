d4387eb36cdb42dcdf07e8521ff15d4868dbbf1b
Soliyev Tolkinxon <krosstony639@gmail.com>
2024-10-28 14:33:50 +0500

    Finish modes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const elFormSelectWrapper = document.querySelector('.js-form__select-wrapper');
+const elModes = document.querySelector('.js-header-modes');
+const mode = getItem('mode') || '';
 
+if(mode){
+    document.body.classList.add('dark');
+}
 
-
+elModes.addEventListener('click',() => {document.body.classList.toggle('dark'); setItem('mode', document.body.classList.value);})
 elFormSelectWrapper.addEventListener('click',() => elFormSelectWrapper.children[0].classList.toggle('rotate'))
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,2 @@
 let setItem = (key, val) => window.localStorage.setItem(key, typeof val == "object" ? JSON.stringify(val) : val);
 let getItem = (key) => window.localStorage.getItem(key);
-setItem('admin',{password: 123, name: 'ali'})
